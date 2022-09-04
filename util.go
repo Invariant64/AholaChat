b4e21667b4ba09d44df93be77d21fa029aae7c20
Invariant64 <98544022+Invariant64@users.noreply.github.com>
2022-09-04 19:15:40 +0800

    Update util.go

--- util.go
+++ util.go
@@ -18,10 +18,5 @@ func session(w http.ResponseWriter, r *http.Request) (sess data.Session, err err
 }
 
 func generateHTML(w http.ResponseWriter, data interface{}, fn ...string) {
-	var files []string
-	for _, file := range fn {
-		files = append(files, fmt.Sprintf("templates/%s.html"), file)
-	}
-	templates := template.Must(template.ParseFiles(files...))
-	templates.ExecuteTemplate(w, "layout", data)
+	
 }
